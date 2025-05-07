b617394a0ef9931607720603e423edde27a57aab
HighWay <lkmsdf159@gmil.com>
2025-05-07 17:48:26 +0900

    25-05-07 KM 수정, 페이지 삭제

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -14,12 +14,6 @@ const menuData: Menu[] = [
     newTab: false,
   },
   {
-    id: 33,
-    title: "Blog",
-    path: "/blog",
-    newTab: false,
-  },
-  {
     id: 3,
     title: "Support",
     path: "/contact",
@@ -43,24 +37,6 @@ const menuData: Menu[] = [
         newTab: false,
       },
       {
-        id: 43,
-        title: "Blog Grid Page",
-        path: "/blog",
-        newTab: false,
-      },
-      {
-        id: 44,
-        title: "Blog Sidebar Page",
-        path: "/blog-sidebar",
-        newTab: false,
-      },
-      {
-        id: 45,
-        title: "Blog Details Page",
-        path: "/blog-details",
-        newTab: false,
-      },
-      {
         id: 46,
         title: "Sign In Page",
         path: "/signin",
@@ -72,12 +48,7 @@ const menuData: Menu[] = [
         path: "/signup",
         newTab: false,
       },
-      {
-        id: 48,
-        title: "Error Page",
-        path: "/error",
-        newTab: false,
-      },
+
     ],
   },
 ];
